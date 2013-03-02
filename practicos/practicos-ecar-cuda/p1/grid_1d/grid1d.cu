7753a73c1385fb0277e95ae7d664c02cc276382a
Emasa <emalomas@gmail.com>
2013-03-02 10:04:55 -0300

    Corregido enunciado de grid1

--- practicos/practicos-ecar-cuda/p1/grid_1d/grid1d.cu
+++ practicos/practicos-ecar-cuda/p1/grid_1d/grid1d.cu
@@ -50,23 +50,18 @@ int main(int argc, char ** argv) {
     // pedir memoria en la placa para la imagen e inicializarla con ceros
     rgba * device_gradient;
     // FALTA: pedir memoria en la placa
-    cutilSafeCall(cudaMalloc(&device_gradient, GRADIENT_BYTES));
     // FALTA: inicializar a 0 la memoria en la placa
-    cutilSafeCall(cudaMemset(device_gradient, 0, GRADIENT_SIZE));
     // correr kernel
     dim3 block(BLOCK_SIZE);         // bloque
     // FALTA: definir el tamaño del grid de acuerdo al tamaño del bloque
     dim3 grid(DIV_CEIL(GRADIENT_SIZE, BLOCK_SIZE));
     // FALTA: llamar al kernel
-    gradient_1d<<<grid, block>>>(device_gradient, GRADIENT_SIZE);
 
     // verificar errores
     cutilCheckMsg("Fallo al lanzar el kernel:");
 
     // FALTA: esperar a que el kernel termine
-    cudaDeviceSynchronize();
     // FALTA: copiar la imagen al host
-    cutilSafeCall(cudaMemcpy(host_gradient, device_gradient, GRADIENT_BYTES, cudaMemcpyDefault));
 
     // inicializar gráficos, dibujar en pantalla
     sdls_init(GRADIENT_SIZE, IMAGE_HEIGHT);
@@ -81,7 +76,6 @@ int main(int argc, char ** argv) {
 
     // limpieza de memoria
     free(host_gradient);
-    cutilSafeCall(cudaFree(device_gradient));
 
     return 0;
 }
